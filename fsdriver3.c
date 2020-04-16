3399b7f7952d408e42a80a7fc1ca4e7302389d67
Matthew Stephens <mattsguitar74@gmail.com>
2020-04-15 17:08:51 -0700

    added documentation describing what's happening in main

--- fsdriver3.c
+++ fsdriver3.c
@@ -9,6 +9,7 @@
 
 int main (int argc, char *argv[])
 {
+    //when we run the driver we specify the volume/root directory name, volume size and block size.
     char * filename;
     uint64_t volumeSize;
     uint64_t blockSize;
@@ -20,25 +21,24 @@ int main (int argc, char *argv[])
         volumeSize = atoll (argv[2]);
         blockSize = atoll (argv[3]);
     }
-
+    //start partition system creates our volume
     retVal = startPartitionSystem (filename, &volumeSize, &blockSize);
     printf("Opened %s, Volume Size: %llu;  BlockSize: %llu; Return %d\n", filename, (ull_t)volumeSize, (ull_t)blockSize, retVal);
+
+    //this is where we will accept user input and call functions
+    loop();
 
     return EXIT_SUCCESS;
     }
 
 
 
-    void loop(char* prompt){
+    void loop(){
         int stat;
         char *line;
         char *args;
         do {
-            if (prompt != NULL){
-                printf("%s ",prompt);
-            } else {
                 printf("> ");
-            }
 
             //make a little loop to accept user input
             char* commands = scanf();
