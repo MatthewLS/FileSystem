0e8cc342cea4512abf980f88a5100b86819307da
Benjamin Lewis <benjaminlewis984@gmail.com>
2020-04-22 16:47:14 -0700

    Created initRoot() function

--- fsdriver3.c
+++ fsdriver3.c
@@ -16,32 +16,73 @@
 #include "Directory.h"
 #include <time.h>
 #include <assert.h>
+
+#define AVGDIRECTORYENTRIES 50
 #define EXIT_SUCCESS 0
 #define MAXCOMMLIST 5
 #define BUFFER_LENGTH 256
 #define BLOCKSIZE 512
+#define DIR_USED 0x00000001
+#define DIR_UNUSED 0x00000002
+#define DIR_SPECIAL 0x00000004
 
-void mkdir(char *);
+
+//void mkdir(char *);
 void loop();
 void addFile(char *, char*);
+void initRoot(uint64_t, uint64_t);
 
 typedef struct openFileEntry {
     int flags;
     uint64_t size;
     uint64_t pointer;
     uint64_t Id;
+    uint64_t position;
+    uint64_t offset;
     char* filebuffer;
 }openFileEntry, *openFileEntry_ptr;
 
 openFileEntry * openFileList;
+
+void initRoot(uint64_t startLocation, uint64_t blockSize){
+    dirEntry_p rootDirPtr;
+
+    uint64_t entrySize = sizeof(dirEntry);
+    uint64_t bytesNeeded = AVGDIRECTORYENTRIES * entrySize;
+    uint64_t blocksNeeded = (bytesNeeded + (blockSize - 1)) / blockSize;
+    uint64_t actualDirEntries = (blocksNeeded * blockSize) / entrySize;
+
+    printf("For %d entries. We need %llu bytes. Each entry is %llu bytes \n",
+            AVGDIRECTORYENTRIES, bytesNeeded, entrySize);
+    rootDirPtr = malloc(blocksNeeded * blockSize);
+
+
+    for (int i = 0; i < actualDirEntries; i++){
+        strcpy(rootDirPtr[i].name, "");
+        rootDirPtr[i].id = 1000;
+        rootDirPtr[i].date = 0;
+        rootDirPtr[i].location = 0;
+        rootDirPtr[i].sizeInBytes = 0;
+        rootDirPtr[i].flags = DIR_UNUSED;
+    }
+    strcpy(rootDirPtr[0].name, "..");
+    rootDirPtr[0].id = 1000;
+    rootDirPtr[0].date = 0;
+    rootDirPtr[0].location = 0;
+    rootDirPtr[0].sizeInBytes = 0;
+    rootDirPtr[0].flags = DIR_UNUSED;
+
+    LBAwrite(rootDirPtr, blocksNeeded, startLocation);
+
+}
 
 uint64_t writeFile(int fd, char *source, uint64_t length){
 //    fd = destination
 //    source = file we are writing from
 //    fd checks -
 
-    uint64_t currentBlock = openFileEntry->position / BLOCKSIZE;
-    uint64_t currentOffset = openFileEntry->offset % BLOCKSIZE;
+    uint64_t currentBlock = openFileList[fd].position / BLOCKSIZE;
+    uint64_t currentOffset = openFileList[fd].offset % BLOCKSIZE;
 
 //    memcpy initilizes the buffer
     if (length + currentOffset < BLOCKSIZE){  //can it fit in one block?
@@ -50,7 +91,10 @@ uint64_t writeFile(int fd, char *source, uint64_t length){
     }
     else if (length + currentOffset < (BLOCKSIZE * 2)){   //does it fit in 2 blocks?
         memcpy(openFileList[fd].filebuffer + currentOffset, src, length);
-        LBAwrite(openFIleLIst[fd].filebuffer + currentOffset, src, length);
+
+//      Check to see if Write or read is beyond the capacity of the volume
+//      uint64_t LBAwrite (void * buffer, uint64_t lbaCount, uint64_t lbaPosition)(
+        LBAwrite(openFileList[fd].filebuffer + currentOffset, src, length);
     } else {  //if it spills over into 3 blocks
 //        Memcopy arguments = source, fd, length, currentOffset
 //        LBAWrite = fileBuffer, blockStart
@@ -65,6 +109,7 @@ int main(int argc, char *argv[])
     uint64_t volumeSize;
     uint64_t blockSize;
     int retVal;
+    openFileEntry openFileList[50];
 
     if (argc > 3)
     {
@@ -73,8 +118,11 @@ int main(int argc, char *argv[])
         blockSize = atoll(argv[3]);
     }
     //start partition system creates our volume
-    //retVal = startPartitionSystem (filename, &volumeSize, &blockSize);
+    retVal = startPartitionSystem (filename, &volumeSize, &blockSize);
     printf("Opened %s, Volume Size: %llu;  BlockSize: %llu; Return %d\n", filename, (ull_t)volumeSize, (ull_t)blockSize, retVal);
+
+    initRoot(0, blockSize);
+
 
     //this is where we will accept user input and call functions
     loop();
@@ -210,7 +258,8 @@ void loop()
             char currWD[PATHMAX];
             char * filename = command[1];
 //      write filename "adsf asdf as asdf"
-            FILE* fd = fopen(filename, "w");
+            int fd;
+            fd = open(filename, "w");
             getcwd(currWD, sizeof(currWD));
 
             size_t n = sizeof(command)/sizeof(command[0]);
